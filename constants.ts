8f02be4e84ac1d0dd293e775c866df9ecdecb3da
K.Minh <trankhaiminh2706@gmail.com>
2026-01-05 00:49:15 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -99,5 +99,8 @@ export const PERSONAL_PROJECTS: Project[] = [
 ];
 
 export const HERO_IMAGES = [
-  "https://plus.unsplash.com/premium_photo-1681487767138-ddf2d67b35c1?q=80&w=2155&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  "https://plus.unsplash.com/premium_photo-1681487767138-ddf2d67b35c1?q=80&w=2155&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1681586533774-1d9d42425712?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZGF0YXxlbnwwfHwwfHx8MA%3D%3D",
+  "https://images.unsplash.com/photo-1642952469120-eed4b65104be?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YWxnb3JpdGhtfGVufDB8fDB8fHww"
+  
 ];
